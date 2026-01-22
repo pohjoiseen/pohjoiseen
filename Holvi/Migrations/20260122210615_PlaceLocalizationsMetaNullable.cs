e5693c23811179d48834e142c542583ac8999d4d
Alexander Ulyanov <procyonar@gmail.com>
2026-01-22 23:14:12 +0200

    Holvi: add useless migration to make EF Core happy

---
+++ Holvi/Migrations/20260122210615_PlaceLocalizationsMetaNullable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Holvi.Migrations
+{
+    /// <inheritdoc />
+    public partial class PlaceLocalizationsMetaNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Meta",
+                table: "PlaceLocalizations",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "{}",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Meta",
+                table: "PlaceLocalizations",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
